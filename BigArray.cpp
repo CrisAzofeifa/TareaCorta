bd230b45190c09a00285464fc20bde5aec0a03e9
Kimc17 <31198587+Kimc17@users.noreply.github.com>
2018-03-18 01:40:32 -0600

    BigArray corregido

--- BigArray.cpp
+++ BigArray.cpp
@@ -5,18 +5,24 @@
 
 using namespace std;
 void BigArray:: Generar() {
-    remove ( "BigArray.txt" );
+    remove ( "BigArray.txt");
     fichero.open("BigArray.txt",ios::app);
 
     srand(time(0));
     int Seed = rand();
     srand(Seed);
     int num, c;
+
     for (c = 1; c <= 5; c++) { //Ampliar cantidad de numeros
+
         num = 1 + rand() % ( 5- 2000);    // Cambiar a numeros mas grandes XD
         cout << num <<  endl;
+        aFichero(num);
 
     }
+
+
+
     fichero.close();
 
 }
