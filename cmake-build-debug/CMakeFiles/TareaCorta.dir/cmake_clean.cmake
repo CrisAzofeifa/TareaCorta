3269b5b12a8ce04feab7510ab7e2004cd9c06ee0
Unknown <azofeifacris4@gmail.com>
2018-03-17 18:03:58 -0600

    Commit 1

---
+++ cmake-build-debug/CMakeFiles/TareaCorta.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TareaCorta.dir/main.cpp.o"
+  "TareaCorta.pdb"
+  "TareaCorta"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TareaCorta.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
